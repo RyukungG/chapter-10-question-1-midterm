c44ebf2aa5d9a9fefecbdb10c7cdc395c0ceb38e
RyukungG <ryu.nosuke@hotmail.com>
2025-02-26 11:36:04 +0700

    update html

--- scripts/local-kub/deploy.sh
+++ scripts/local-kub/deploy.sh
@@ -9,13 +9,13 @@
 #
 # Build Docker images.
 #
-docker build -t metadata:1 --file ../../metadata/Dockerfile-prod ../../metadata
-docker build -t history:1 --file ../../history/Dockerfile-prod ../../history
-docker build -t mock-storage:1 --file ../../mock-storage/Dockerfile-prod ../../mock-storage
-docker build -t history:1 --file ../../history/Dockerfile-prod ../../history
-docker build -t video-streaming:1 --file ../../video-streaming/Dockerfile-prod ../../video-streaming
-docker build -t video-upload:1 --file ../../video-upload/Dockerfile-prod ../../video-upload
-docker build -t gateway:1 --file ../../gateway/Dockerfile-prod ../../gateway
+docker build -t metadata:1 --file ../metadata/Dockerfile-prod ./metadata
+docker build -t history:1 --file ./history/Dockerfile-prod ./history
+docker build -t mock-storage:1 --file ./mock-storage/Dockerfile-prod ./mock-storage
+docker build -t history:1 --file ./history/Dockerfile-prod ./history
+docker build -t video-streaming:1 --file ./video-streaming/Dockerfile-prod ./video-streaming
+docker build -t video-upload:1 --file ./video-upload/Dockerfile-prod ./video-upload
+docker build -t gateway:1 --file ./gateway/Dockerfile-prod ./gateway
 
 # 
 # Deploy containers to Kubernetes.
@@ -31,4 +31,5 @@ kubectl apply -f history.yaml
 kubectl apply -f mock-storage.yaml
 kubectl apply -f video-streaming.yaml
 kubectl apply -f video-upload.yaml
+kubectl apply -f advertise.yaml
 kubectl apply -f gateway.yaml
